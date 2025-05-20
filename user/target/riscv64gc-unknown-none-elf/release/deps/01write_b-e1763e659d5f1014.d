c347d9045885cec5c38f505f29b6db5bc73f5aaa
王明辉 <13293532+trisenna@user.noreply.gitee.com>
2025-05-20 22:44:46 +0800

    22301022 第五次实验，第一步

---
+++ user/target/riscv64gc-unknown-none-elf/release/deps/01write_b-e1763e659d5f1014.d
@@ -0,0 +1,5 @@
+/mnt/user/target/riscv64gc-unknown-none-elf/release/deps/01write_b-e1763e659d5f1014.d: src/bin/01write_b.rs
+
+/mnt/user/target/riscv64gc-unknown-none-elf/release/deps/01write_b-e1763e659d5f1014: src/bin/01write_b.rs
+
+src/bin/01write_b.rs:
---
+++ user/target/riscv64gc-unknown-none-elf/release/deps/02write_c-f03d1a8a066fa228.d
@@ -0,0 +1,5 @@
+/mnt/user/target/riscv64gc-unknown-none-elf/release/deps/02write_c-f03d1a8a066fa228.d: src/bin/02write_c.rs
+
+/mnt/user/target/riscv64gc-unknown-none-elf/release/deps/02write_c-f03d1a8a066fa228: src/bin/02write_c.rs
+
+src/bin/02write_c.rs:
