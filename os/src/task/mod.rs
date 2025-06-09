bc084a3a9e10c482267ed85d6cd49b5daeed0681
王明辉 <13293532+trisenna@user.noreply.gitee.com>
2025-06-09 15:16:42 +0800

    22301022 第七次实验，第9次提交

--- os/src/task/mod.rs
+++ os/src/task/mod.rs
@@ -14,8 +14,7 @@ use crate::sync::UPSafeCell;
 pub use manager::{add_task, fetch_task, TaskManager};
 pub use processor::{
     run_tasks, current_task, current_user_token, current_trap_cx,
-    schedule, take_current_task, suspend_current_and_run_next,
-    exit_current_and_run_next,
+    schedule, take_current_task,
 };
 pub use pid::{PidHandle, pid_alloc, KernelStack, kernel_stack_position};
 pub use switch::__switch;
@@ -28,4 +27,53 @@ lazy_static! {
 
 pub fn add_initproc() {
     add_task(INITPROC.clone());
+}
+
+pub fn suspend_current_and_run_next() {
+    // There must be an application running.
+    let task = take_current_task().unwrap();
+    // ---- access current TCB exclusively
+    let mut task_inner = task.inner_exclusive_access();
+    let task_cx_ptr = &mut task_inner.task_cx as *mut TaskContext;
+    // Change status to Ready
+    task_inner.task_status = TaskStatus::Ready;
+    drop(task_inner);
+    // ---- release current PCB
+    // push back to ready queue.
+    add_task(task);
+    // jump to scheduling cycle
+    schedule(task_cx_ptr);
+}
+
+pub fn exit_current_and_run_next() {
+    // take from Processor
+    let task = take_current_task().unwrap();
+    // **** access current TCB exclusively
+    let mut inner = task.inner_exclusive_access();
+    // Change status to Zombie
+    inner.task_status = TaskStatus::Zombie;
+    // Record exit code
+    inner.exit_code = 0;
+    // do not move to its parent but under initproc
+
+    // ++++++ access initproc TCB exclusively
+    {
+        let mut initproc_inner = INITPROC.inner_exclusive_access();
+        for child in inner.children.iter() {
+            child.inner_exclusive_access().parent = Some(Arc::downgrade(&INITPROC));
+            initproc_inner.children.push(child.clone());
+        }
+    }
+    // ++++++ release parent PCB
+
+    inner.children.clear();
+    // deallocate user space
+    inner.memory_set.recycle_data_pages();
+    drop(inner);
+    // **** release current PCB
+    // drop task manually to maintain rc correctly
+    drop(task);
+    // we do not have to save task context
+    let mut _unused = TaskContext::zero_init();
+    schedule(&mut _unused as *mut _);
 }
