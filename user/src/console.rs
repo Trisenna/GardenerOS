12993e5fdaa3074b199e38bc04b9f8157cf7ed41
王明辉 <13293532+trisenna@user.noreply.gitee.com>
2025-06-09 14:56:54 +0800

    22301022 第七次实验，第一次提交

--- user/src/console.rs
+++ user/src/console.rs
@@ -1,7 +1,14 @@
+use super::{read, write, exit};
 use core::fmt::{self, Write};
-use super::write;
 
+const STDIN: usize = 0;
 const STDOUT: usize = 1;
+
+pub fn getchar() -> u8 {
+    let mut c = [0u8; 1];
+    read(STDIN, &mut c);
+    c[0]
+}
 
 struct Stdout;
 
@@ -30,3 +37,14 @@ macro_rules! println {
     }
 }
 
+#[panic_handler]
+fn panic_handler(panic_info: &core::panic::PanicInfo) -> ! {
+    if let Some(location) = panic_info.location() {
+        println!("Panicked at {}:{}", location.file(), location.line());
+    } else {
+        println!("Panicked!");
+    }
+    let message = panic_info.message();
+    println!("{}", message);
+    exit(-1);
+}
